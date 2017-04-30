a5db41e95aeff4d4ca1adebc4eab5f47cd06b3a2
rr- <rr-@sakuya.pl>
2017-04-30 19:16:17 +0200

    config: add user configs; configurable credentials

--- soladm/__main__.py
+++ soladm/__main__.py
@@ -1,28 +1,59 @@
 from getpass import getpass
 import argparse
+from typing import Optional, Tuple
 from pathlib import Path
 from soladm import config
 from soladm import net
 from soladm import ui
 
 
+DEFAULT_PORT = 23073
+
+
 def parse_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser('Soldat admin client')
-    parser.add_argument('host')
-    parser.add_argument('port', type=int)
-    parser.add_argument('password', default='', nargs='?')
+    parser.add_argument('-c', '--config', help='path to optional config file')
+    parser.add_argument(
+        '--host', help='ip or hostname to connect to')
+    parser.add_argument(
+        '--port', type=int, help='default: {}'.format(DEFAULT_PORT))
+    parser.add_argument(
+        '--pass', dest='password', default='',
+        help='if ommitted, you\'ll be asked for it interactively')
     return parser.parse_args()
+
+
+def _load_config(user_path: Optional[str]) -> None:
+    config.read_config(
+        Path(__file__).parent.joinpath('data', 'default_config.ini'))
+    if user_path:
+        config.read_config(Path(user_path))
+
+
+def _get_connection_info(args: argparse.Namespace) -> Tuple[str, int, str]:
+    cfg = config.get_config()
+
+    host: Optional[str] = None
+    host = args.host or cfg.connection.host
+    while not host:
+        host = input('Enter host: ')
+
+    port: int = args.port or cfg.connection.port or DEFAULT_PORT
+
+    password: Optional[str] = args.password or cfg.connection.password
+    while not password:
+        password = getpass('Enter password: ')
+
+    assert host
+    assert port
+    assert password
+    return (host, port, password)
 
 
 def main() -> None:
     args = parse_args()
-    host: str = args.host
-    port: int = args.port
-    password: str = args.password or getpass('Enter password: ')
-
-    config.read_config(
-        Path(__file__).parent.joinpath('data', 'default_config.ini'))
-
+    _load_config(args.config)
+    host, port, password = _get_connection_info(args)
     connection = net.Connection(host, port, password)
     ui.run(connection)
 
--- soladm/config.py
+++ soladm/config.py
@@ -1,5 +1,5 @@
 import configparser
-from typing import List
+from typing import Optional, List
 from pathlib import Path
 
 
@@ -10,9 +10,17 @@ class AutoCompleteConfig:
         self.bot_names: List[str] = []
 
 
+class ConnectionConfig:
+    def __init__(self) -> None:
+        self.host: Optional[str] = None
+        self.port: Optional[int] = None
+        self.password: Optional[str] = None
+
+
 class Config:
     def __init__(self) -> None:
         self.autocomplete = AutoCompleteConfig()
+        self.connection = ConnectionConfig()
 
 
 _config = Config()
@@ -22,7 +30,7 @@ def _split_lines(text: str) -> List[str]:
     return [line.strip() for line in text.split('\n')]
 
 
-def read_config(path: Path):
+def read_config(path: Path) -> None:
     ini = configparser.ConfigParser(interpolation=None)
     ini.read_string(path.read_text())
 
@@ -32,6 +40,11 @@ def read_config(path: Path):
         ini.get('autocomplete', 'map_names', fallback=''))
     _config.autocomplete.bot_names = _split_lines(
         ini.get('autocomplete', 'bot_names', fallback=''))
+    _config.connection.host = ini.get('server', 'host', fallback=None)
+    _config.connection.port = ini.getint('server', 'port', fallback=None)
+    _config.connection.password = (
+        ini.get('server', 'pass', fallback=None) or
+        ini.get('server', 'password', fallback=None))
 
 
 def get_config() -> Config:
