bbf979062b79f419ef9576416cc102525c1aeef2
rr- <rr-@sakuya.pl>
2017-04-30 17:30:00 +0200

    ui: add map names to autocompletion

--- soladm/ui/autocomplete.py
+++ soladm/ui/autocomplete.py
@@ -68,6 +68,107 @@ SERVER_COMMANDS = [
 ]
 
 
+MAP_NAMES = [
+    'Aero',
+    'Airpirates',
+    'Arena2',
+    'Arena3',
+    'Arena',
+    'Bigfalls',
+    'Blox',
+    'Bridge',
+    'Bunker',
+    'Cambodia',
+    'CrackedBoot',
+    'ctf_Ash',
+    'ctf_B2b',
+    'ctf_Blade',
+    'ctf_Campeche',
+    'ctf_Cobra',
+    'ctf_Crucifix',
+    'ctf_Death',
+    'ctf_Division',
+    'ctf_Dropdown',
+    'ctf_Equinox',
+    'ctf_Guardian',
+    'ctf_Hormone',
+    'ctf_IceBeam',
+    'ctf_Kampf',
+    'ctf_Lanubya',
+    'ctf_Laos',
+    'ctf_Mayapan',
+    'ctf_Maya',
+    'ctf_MFM',
+    'ctf_Nuubia',
+    'ctf_Raspberry',
+    'ctf_Rotten',
+    'ctf_Ruins',
+    'ctf_Run',
+    'ctf_Scorpion',
+    'ctf_Snakebite',
+    'ctf_Steel',
+    'ctf_Triumph',
+    'ctf_Viet',
+    'ctf_Voland',
+    'ctf_Wretch',
+    'ctf_X',
+    'Daybreak',
+    'DesertWind',
+    'Factory',
+    'Flashback',
+    'HH',
+    'htf_Arch',
+    'htf_Baire',
+    'htf_Boxed',
+    'htf_Desert',
+    'htf_Dorothy',
+    'htf_Dusk',
+    'htf_Erbium',
+    'htf_Feast',
+    'htf_Mossy',
+    'htf_Muygen',
+    'htf_Niall',
+    'htf_Nuclear',
+    'htf_Prison',
+    'htf_Rubik',
+    'htf_Star',
+    'htf_Tower',
+    'htf_Void',
+    'htf_Vortex',
+    'htf_Zajacz',
+    'inf_Abel',
+    'inf_April',
+    'inf_Argy',
+    'inf_Belltower',
+    'inf_Biologic',
+    'inf_Changeling',
+    'inf_Flute',
+    'inf_Fortress',
+    'inf_Industrial',
+    'inf_Messner',
+    'inf_Moonshine',
+    'inf_Motheaten',
+    'inf_Outpost',
+    'inf_Rescue',
+    'inf_Rise',
+    'inf_Warehouse',
+    'inf_Warlock',
+    'Island2k5',
+    'Jungle',
+    'Krab',
+    'Lagrange',
+    'Leaf',
+    'MrSnowman',
+    'RatCave',
+    'Rok',
+    'RR',
+    'Shau',
+    'Tropiccave',
+    'Unlim',
+    'Veoto',
+]
+
+
 def get_affixes(
         edit_text: str,
         edit_pos: int) -> List[Tuple[str, str, str]]:
@@ -100,9 +201,12 @@ def collect(
                     player.name.lower().startswith(infix.lower()):
                 yield prefix + player.name + suffix
 
-        for command in SERVER_COMMANDS:
-            if prefix == '' and command.lower().startswith(infix.lower()):
-                if suffix:
-                    yield command + suffix
-                else:
-                    yield command + ' '
+        if not prefix:
+            for command in SERVER_COMMANDS:
+                if command.lower().startswith(infix.lower()):
+                    yield command + (suffix or ' ')
+
+        if prefix.strip().lower() == '/map':
+            for map_name in MAP_NAMES:
+                if map_name.lower().startswith(infix.lower()):
+                    yield prefix + map_name + suffix
